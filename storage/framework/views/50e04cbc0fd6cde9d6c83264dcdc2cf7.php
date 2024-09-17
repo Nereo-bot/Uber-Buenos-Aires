5f7248e12a23f133330401b4a17d2ed686efb842
Leandro Ferrari <leandroferrarimunoz@gmail.com>
2024-09-17 15:41:35 -0300

    Actualizar 50e04cbc0fd6cde9d6c83264dcdc2cf7.php

--- storage/framework/views/50e04cbc0fd6cde9d6c83264dcdc2cf7.php
+++ storage/framework/views/50e04cbc0fd6cde9d6c83264dcdc2cf7.php
@@ -32,4 +32,4 @@
         </div>
     </body>
 </html>
-<?php /**PATH /home/andrade/Documents/Proyectos/php/uber/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/minimal.blade.php ENDPATH**/ ?>
+<?php /**PATH /home/leandro/Documents/Proyectos/php/uber/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/minimal.blade.php ENDPATH**/ ?>
