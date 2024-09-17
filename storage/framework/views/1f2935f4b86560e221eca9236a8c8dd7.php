6c934c11eeb8acd6d66d108d45916dc394d3149d
Leandro Ferrari <leandroferrarimunoz@gmail.com>
2024-09-17 15:30:56 -0300

    Actualizar 1f2935f4b86560e221eca9236a8c8dd7.php

--- storage/framework/views/1f2935f4b86560e221eca9236a8c8dd7.php
+++ storage/framework/views/1f2935f4b86560e221eca9236a8c8dd7.php
@@ -260,4 +260,4 @@
 <?php if (isset($__componentOriginal69dc84650370d1d4dc1b42d016d7226b)): ?>
 <?php $component = $__componentOriginal69dc84650370d1d4dc1b42d016d7226b; ?>
 <?php unset($__componentOriginal69dc84650370d1d4dc1b42d016d7226b); ?>
-<?php endif; ?><?php /**PATH /home/andrade/Documents/Proyectos/php/uber/resources/views/auth/registro.blade.php ENDPATH**/ ?>
+<?php endif; ?><?php /**PATH /home/leandro/Documents/Proyectos/php/uber/resources/views/auth/registro.blade.php ENDPATH**/ ?>
